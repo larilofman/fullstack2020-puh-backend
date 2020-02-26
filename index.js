7510b882b445694b1bb9f112c6569a34cc94758c
Lari Löfman <larwazor@gmail.com>
2020-02-26 12:26:43 +0200

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -27,6 +28,7 @@ let persons = [
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
